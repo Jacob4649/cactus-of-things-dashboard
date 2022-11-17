eb7f08e1de08290a97374f40f61720970e1d5b50
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-17 18:33:42 -0500

    make dashboard page index

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route index element={<Dashboard />} />
         <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
