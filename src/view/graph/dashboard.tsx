64fc97c9a0c291302c59b6fc226ff61bc59a93b1
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-17 18:57:59 -0500

    add graph title

--- src/view/graph/dashboard.tsx
+++ src/view/graph/dashboard.tsx
@@ -1,10 +1,12 @@
 import { ReadingGraph } from "./readingGraph";
+import './dashboard.css';
 
 /**
  * Dashboard for cactus-of-things
  */
 export default function Dashboard() {
     return <>
+        <h1>Moisture Level of Cactus</h1>
         <ReadingGraph scale="day"></ReadingGraph>
     </>
 }
